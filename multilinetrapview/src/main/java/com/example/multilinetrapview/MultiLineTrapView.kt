1a965be235f7439952b0398ae2a992e7f7834627
Anwesh43 <anweshthecool0@gmail.com>
2020-12-27 03:45:02 +0530

    adding Int and Float extension methods

--- multilinetrapview/src/main/java/com/example/multilinetrapview/MultiLineTrapView.kt
+++ multilinetrapview/src/main/java/com/example/multilinetrapview/MultiLineTrapView.kt
@@ -24,3 +24,8 @@ val trapFactor : Float = 8.7f
 val strokeFactor : Float = 90f
 val parts : Int = 4
 val scGap : Float = 0.02f / parts
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
