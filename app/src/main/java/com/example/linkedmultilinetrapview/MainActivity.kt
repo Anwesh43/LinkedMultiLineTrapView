ed0becbb86f18b554ce3c5624a29ff2e869d6f65
Anwesh43 <anweshthecool0@gmail.com>
2020-12-27 04:23:58 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedmultilinetrapview/MainActivity.kt
+++ app/src/main/java/com/example/linkedmultilinetrapview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedmultilinetrapview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.multilinetrapview.MultiLineTrapView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MultiLineTrapView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
