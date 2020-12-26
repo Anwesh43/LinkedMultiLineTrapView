77beed5cea93f5248e3adb654bbfe37193a5cf61
Anwesh43 <anweshthecool0@gmail.com>
2020-12-27 04:13:01 +0530

    adding MLTNode class

--- multilinetrapview/src/main/java/com/example/multilinetrapview/MultiLineTrapView.kt
+++ multilinetrapview/src/main/java/com/example/multilinetrapview/MultiLineTrapView.kt
@@ -132,4 +132,45 @@ class MultiLineTrapView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MLTNode(var i : Int, val state : State = State()) {
+
+        private var next : MLTNode? = null
+        private var prev : MLTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MLTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMLTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MLTNode {
+            var curr : MLTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
