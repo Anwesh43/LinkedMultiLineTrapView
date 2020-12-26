a2eafeb2099b5be136c34236f5018c4e05727211
Anwesh43 <anweshthecool0@gmail.com>
2020-12-27 04:20:58 +0530

    adding renderer in view

--- multilinetrapview/src/main/java/com/example/multilinetrapview/MultiLineTrapView.kt
+++ multilinetrapview/src/main/java/com/example/multilinetrapview/MultiLineTrapView.kt
@@ -72,14 +72,16 @@ fun Canvas.drawMLTNode(i : Int, scale : Float, paint : Paint) {
 
 class MultiLineTrapView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -203,7 +205,7 @@ class MultiLineTrapView(ctx : Context) : View(ctx) {
         private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
         private val animator : Animator = Animator(view)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             mlt.draw(canvas, paint)
             animator.animate {
