5f63a186521b5aa7160d5540cf420a7cddebb4f5
Anwesh43 <anweshthecool0@gmail.com>
2020-12-27 04:04:02 +0530

    adding MultiLineTrapView class

--- multilinetrapview/src/main/java/com/example/multilinetrapview/MultiLineTrapView.kt
+++ multilinetrapview/src/main/java/com/example/multilinetrapview/MultiLineTrapView.kt
@@ -69,3 +69,19 @@ fun Canvas.drawMLTNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawMultiLineTrap(scale, w, h, paint)
 }
+
+class MultiLineTrapView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
